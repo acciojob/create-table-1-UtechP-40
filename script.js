37ddf2bc1de00e73677310e8c05aa26b3e5ab5ca
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-14 12:29:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table = document.querySelector("#sampleTable")
+  let tr = table.insertRow();
+  let td = tr.insertCell()
+	td.innerText ="Row3 cell1"
+	let td2 = tr.insertCell()
+	td2.innerText ="Row3 cell2"
 }
